ca557a6f858b87aed298e8f397e8b5cf1de7c5e6
anacle33 <48806914+anacle33@users.noreply.github.com>
2019-03-22 19:57:51 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ module.exports = async(client) => {
 
     client.user.setPresence({
         game: {
-            name: "Je me fais coder"
+            name: "Anacle me chauffe haaaaa"
         }
     })
  };
